a1bfaeab5d8eb145166b00c307b89c97e6b83bd6
Jarred Glaser <jarred.glaser@gmail.com>
2021-04-26 18:23:46 +0000

    finish modules

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -1,18 +1,16 @@
 // modules2.rs
 // Make me compile! Execute `rustlings hint modules2` for hints :)
 
-// I AM NOT DONE
+pub mod delicious_snacks {
+    pub use self::fruits as fruit;
+    pub use self::veggies as veggie;
 
-mod delicious_snacks {
-    use self::fruits::PEAR as fruit;
-    use self::veggies::CUCUMBER as veggie;
-
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
@@ -21,7 +19,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        delicious_snacks::fruit::PEAR,
+        delicious_snacks::veggie::CUCUMBER
     );
 }
